5e2a5c1a3df08dd8089104a33adb6a66b3412fdd
Sajjad Shahi <shahi_cse@yahoo.com>
2017-08-13 13:11:44 +0430

    Get Row & Get Column

--- src/main/java/kon/shol/HBase.java
+++ src/main/java/kon/shol/HBase.java
@@ -11,8 +11,9 @@ import org.apache.hadoop.hbase.HColumnDescriptor;
 import org.apache.hadoop.hbase.HTableDescriptor;
 
 import java.io.IOException;
+import java.util.NavigableMap;
 
-public class HBase   {
+public class HBase {
 
 //    // Refer to table metadata names by byte array in the HBase API
 //    private static final byte[] TABLE_NAME = Bytes.toBytes("Hello-Bigtable");
@@ -128,23 +129,49 @@ public class HBase   {
 //        }
 //        return value;
 //    }
-    public static void main(String[] args) throws IOException
-    {
+
+
+//    COMMANDS FOR CREATING TABLE
+
+    //        HTableDescriptor hTableDescriptor = new HTableDescriptor(tableName);
+ /*       hTableDescriptor.addFamily(new HColumnDescriptor("Id"));
+        hTableDescriptor.addFamily(new HColumnDescriptor("Name"));*/
+//        admin.createTable(hTableDescriptor);
+    public static String[] getColumnsInColumnFamily(Result r, String ColumnFamily) {
+
+        NavigableMap<byte[], byte[]> familyMap = r.getFamilyMap(Bytes.toBytes(ColumnFamily));
+        String[] Quantifers = new String[familyMap.size()];
+
+        int counter = 0;
+        for (byte[] bQunitifer : familyMap.keySet()) {
+            Quantifers[counter++] = Bytes.toString(bQunitifer);
+
+        }
+
+        return Quantifers;
+    }
+
+    public static void main(String[] args) throws IOException {
         Configuration configuration = HBaseConfiguration.create();
         Connection connection = ConnectionFactory.createConnection(configuration);
-        Admin admin  = connection.getAdmin();
-        System.out.println( "Connecting..." );
+        Admin admin = connection.getAdmin();
+        System.out.println("Connecting...");
         TableName tableName = TableName.valueOf("myTable");
-        HTableDescriptor hTableDescriptor = new HTableDescriptor(tableName);
-        HTable hTable = new HTable(configuration, tableName);
-        hTableDescriptor.addFamily(new HColumnDescriptor("Id"));
-        hTableDescriptor.addFamily(new HColumnDescriptor("Name"));
-        Put put = new Put(Bytes.toBytes("myRow"));
-        put.addColumn(Bytes.toBytes("Id"), Bytes.toBytes("q1"), Bytes.toBytes(102900));
-        System.out.println( "Creating Table..." );
-        admin.createTable(hTableDescriptor);
-
-        hTable.put(put);
+        Table table = connection.getTable(tableName);
+        Put put = new Put(Bytes.toBytes("myRow3"));
+        put.addColumn(Bytes.toBytes("Name"), Bytes.toBytes("Aida"), Bytes.toBytes(102900));
+        Get get = new Get(Bytes.toBytes("myRow2"));
+        Scan scan = new Scan();
+        System.out.println(table.get(get));
+        System.out.println(table.getScanner(scan).toString());
+        System.out.println("Creating Table...");
+        Result result = table.get(get);
+        System.out.println(result);
+//        table.put(put);
         System.out.println("Done!");
+        byte[] databytes = Bytes.toBytes("Name");
+        System.out.println(getColumnsInColumnFamily(result, "Name")[0]);
+        table.close();
+        connection.close();
     }
 }
