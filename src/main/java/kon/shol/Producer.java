f624e5aa79304f6db45a317bdc0a4dc3f001d184
mohammadreza <ghobakhlou.mohammadreza@gmail.com>
2017-08-14 02:55:59 +0430

    kafka Producer and consumer test

---
+++ src/main/java/kon/shol/Producer.java
@@ -0,0 +1,52 @@
+package kon.shol;
+
+import kafka.admin.AdminUtils;
+import kafka.admin.RackAwareMode;
+import kafka.utils.ZKStringSerializer$;
+import org.I0Itec.zkclient.ZkClient;
+import kafka.utils.ZkUtils;
+import org.I0Itec.zkclient.ZkConnection;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import java.util.Properties;
+import java.util.Scanner;
+import static org.apache.kafka.clients.producer.ProducerConfig.BOOTSTRAP_SERVERS_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG;
+
+public class Producer {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String link = scanner.nextLine();
+        Properties configProperties = new Properties();
+        configProperties.put(BOOTSTRAP_SERVERS_CONFIG, "188.165.235.136:9092");
+        configProperties.put(KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.ByteArraySerializer");
+        configProperties.put(VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        org.apache.kafka.clients.producer.Producer producer = new KafkaProducer<String, String>(configProperties);
+        ProducerRecord<String, String> rec = new ProducerRecord<>("arash", link);
+        producer.send(rec);
+        producer.close();
+//    }
+    }
+
+    public void creatTopic(String topicName, int partitions){
+
+        String zookeeperConnect = "zkserver1:2181,zkserver2:2181";
+        int sessionTimeoutMs = 10 * 1000;
+        int connectionTimeoutMs = 8 * 1000;
+
+        ZkClient zkClient = new ZkClient(
+                zookeeperConnect,
+                sessionTimeoutMs,
+                connectionTimeoutMs,
+                ZKStringSerializer$.MODULE$);
+
+        boolean isSecureKafkaCluster = false;
+
+        ZkUtils zkUtils = new ZkUtils(zkClient, new ZkConnection(zookeeperConnect), isSecureKafkaCluster);
+        Properties topicConfig = new Properties();
+        AdminUtils.createTopic(zkUtils, topicName, partitions, 1, topicConfig, RackAwareMode.Enforced$.MODULE$);
+        zkClient.close();
+    }
+}
