6103b40586d18115928100f7ea22fcb6d5dcf948
Sajjad Shahi <shahi_cse@yahoo.com>
2017-08-15 11:59:32 +0430

    Connect hbae via Java API to server, test methods

--- src/main/java/kon/shol/HBase.java
+++ src/main/java/kon/shol/HBase.java
@@ -133,10 +133,7 @@ public class HBase {
 
 //    COMMANDS FOR CREATING TABLE
 
-    //        HTableDescriptor hTableDescriptor = new HTableDescriptor(tableName);
- /*       hTableDescriptor.addFamily(new HColumnDescriptor("Id"));
-        hTableDescriptor.addFamily(new HColumnDescriptor("Name"));*/
-//        admin.createTable(hTableDescriptor);
+
     public static String[] getColumnsInColumnFamily(Result r, String ColumnFamily) {
 
         NavigableMap<byte[], byte[]> familyMap = r.getFamilyMap(Bytes.toBytes(ColumnFamily));
@@ -152,25 +149,36 @@ public class HBase {
     }
 
     public static void main(String[] args) throws IOException {
+        TableName tableName = TableName.valueOf("myTable");
+     /*   HTableDescriptor hTableDescriptor = new HTableDescriptor(tableName);
+        hTableDescriptor.addFamily(new HColumnDescriptor("Id"));
+        hTableDescriptor.addFamily(new HColumnDescriptor("Name"));
+*/
         Configuration configuration = HBaseConfiguration.create();
+        configuration.set("hbase.zookeeper.quorum", "188.165.230.122:2181");
         Connection connection = ConnectionFactory.createConnection(configuration);
         Admin admin = connection.getAdmin();
+//        admin.createTable(hTableDescriptor);
         System.out.println("Connecting...");
-        TableName tableName = TableName.valueOf("myTable");
+
         Table table = connection.getTable(tableName);
-        Put put = new Put(Bytes.toBytes("myRow3"));
-        put.addColumn(Bytes.toBytes("Name"), Bytes.toBytes("Aida"), Bytes.toBytes(102900));
-        Get get = new Get(Bytes.toBytes("myRow2"));
-        Scan scan = new Scan();
-        System.out.println(table.get(get));
-        System.out.println(table.getScanner(scan).toString());
-        System.out.println("Creating Table...");
-        Result result = table.get(get);
-        System.out.println(result);
+        Put put = new Put(Bytes.toBytes("myRow2"));
+        put.addColumn(Bytes.toBytes("Name"), Bytes.toBytes("firstName"), Bytes.toBytes("Kambiz"));
+        put.addColumn(Bytes.toBytes("Name"), Bytes.toBytes("lastName"), Bytes.toBytes("Gholami"));
 //        table.put(put);
+        Get get = new Get(Bytes.toBytes("myRow6"));
+        System.out.println(table.exists(get));
+//        Scan scan = new Scan();
+        get.setMaxVersions(1);
+        get.addFamily(Bytes.toBytes("Name"));
+        get.addColumn(Bytes.toBytes("Name"), Bytes.toBytes("lastName"));
+
+        Result result = table.get(get);
+        System.out.println(Bytes.toString(result.getValue(Bytes.toBytes("Name"),Bytes.toBytes("lastName"))));
+//        System.out.println(result);
         System.out.println("Done!");
         byte[] databytes = Bytes.toBytes("Name");
-        System.out.println(getColumnsInColumnFamily(result, "Name")[0]);
+//        System.out.println(getColumnsInColumnFamily(result, "Name")[0]);
         table.close();
         connection.close();
     }
