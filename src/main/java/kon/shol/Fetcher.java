9f1d6c71b9b5899fda43f7fd715745084f7c4db4
Hossein Ahmadzadeh <h.ahmadzadeh1998@gmail.com>
2017-08-15 12:13:22 +0430

    Handling exceptions and running local

--- src/main/java/kon/shol/Fetcher.java
+++ src/main/java/kon/shol/Fetcher.java
@@ -4,20 +4,12 @@ import org.jsoup.Jsoup;
 
 public class Fetcher {
     WebPage page = new WebPage();
-    boolean setHTML() {
+    public boolean setHTML() {
         try {
             this.page.html = Jsoup.connect(page.link).get();
             return true;
         } catch (Exception ignore) {
             return false;
         }
-    }
-
-    public static void main(String[] args) {
-
-        Fetcher temp = new Fetcher();
-        temp.page.link = "http://asriran.com";
-        temp.setHTML();
-        System.out.println(temp.page.html.body().text());
     }
 }
