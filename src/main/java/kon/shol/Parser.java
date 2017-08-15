77e41e91df644c9bffed4854811598990c6f4d76
Hossein Ahmadzadeh <h.ahmadzadeh1998@gmail.com>
2017-08-15 12:05:57 +0430

    handled exceptions - getting domain of links

--- src/main/java/kon/shol/Parser.java
+++ src/main/java/kon/shol/Parser.java
@@ -1,11 +1,24 @@
 package kon.shol;
-
+;
+import com.google.common.base.Optional;
+import com.optimaize.langdetect.LanguageDetector;
+import com.optimaize.langdetect.LanguageDetectorBuilder;
+import com.optimaize.langdetect.i18n.LdLocale;
+import com.optimaize.langdetect.ngram.NgramExtractors;
+import com.optimaize.langdetect.profiles.LanguageProfile;
+import com.optimaize.langdetect.profiles.LanguageProfileReader;
+import com.optimaize.langdetect.text.CommonTextObjectFactories;
+import com.optimaize.langdetect.text.TextObject;
+import com.optimaize.langdetect.text.TextObjectFactory;
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
+import com.google.common.base.*;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
 
 public class Parser {
 
@@ -42,5 +55,4 @@ public class Parser {
             return null;
         }
     }
-
 }
