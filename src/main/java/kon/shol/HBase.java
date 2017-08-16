9b7a470d9b5fc80eecb16fcacc34e4903c413d2f
Sajjad Shahi <shahi_cse@yahoo.com>
2017-08-16 13:15:00 +0430

    Implement another way for storing ArrayList using Writable Interface in hadoop

--- src/main/java/kon/shol/HBase.java
+++ src/main/java/kon/shol/HBase.java
@@ -10,8 +10,15 @@ import org.apache.hadoop.hbase.HBaseConfiguration;
 
 import org.apache.hadoop.hbase.HColumnDescriptor;
 import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.io.ArrayWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.io.WritableUtils;
 
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.NavigableMap;
 
@@ -36,6 +43,37 @@ public class HBase {
 
         return Quantifers;
     }
+
+//    THE COMMON WAY TO SERIALIZE LIST AND STORE TO HBASE
+//    TODO: LEARN WRITABLE INTERFACE
+    public static Writable toWritable(ArrayList<String> list) {
+        Writable[] content = new Writable[list.size()];
+        for (int i = 0; i < content.length; i++) {
+            content[i] = new Text(list.get(i));
+        }
+        return new ArrayWritable(Text.class, content);
+    }
+    public static ArrayList<String> fromWritable(ArrayWritable writable) {
+        Writable[] writables = ((ArrayWritable) writable).get();
+        ArrayList<String> list = new ArrayList<String>(writables.length);
+        for (Writable wrt : writables) {
+            list.add(((Text)wrt).toString());
+        }
+        return list;
+    }
+    public static ArrayList<String> castResultToStringArrayList (Result r, String columnFamily, String cell) throws IOException {
+        ArrayWritable arrayWritable = new ArrayWritable(Text.class);
+        arrayWritable.readFields(
+                new DataInputStream(
+                        new ByteArrayInputStream(
+                                r.getValue(Bytes.toBytes(columnFamily), Bytes.toBytes(cell))
+                        )
+                )
+        );
+        return fromWritable(arrayWritable);
+    }
+
+//    A ZAKHAR ALTERNATIVE FOR STORING STRING ARRAY
     private static String[] castResultToStringArray(Result r, String columnFamily, String cell){
         String DBOutput = Bytes.toString(r.getValue(Bytes.toBytes(columnFamily), Bytes.toBytes(cell)));
         Object[] DBOutputArrays = Arrays.stream(DBOutput.substring(1, DBOutput.length() - 1).split(","))
@@ -43,11 +81,8 @@ public class HBase {
         return Arrays.copyOf(DBOutputArrays, DBOutputArrays.length, String[].class);
     }
 
-     /*   HTableDescriptor hTableDescriptor = new HTableDescriptor(tableName);
-        hTableDescriptor.addFamily(new HColumnDescriptor("Id"));
-        hTableDescriptor.addFamily(new HColumnDescriptor("Name"));
-        Admin admin = connection.getAdmin();
-        admin.createTable(hTableDescriptor);*/
+
+
 
     public static void main(String[] args) throws IOException {
         TableName tableName = TableName.valueOf("myTable");
@@ -58,29 +93,21 @@ public class HBase {
         System.out.println("Connecting...");
 
         Table table = connection.getTable(tableName);
-        Put put = new Put(Bytes.toBytes("myRow4"));
-        put.addColumn(Bytes.toBytes("Name"), Bytes.toBytes("firstName"), Bytes.toBytes("Kambiz"));
-        put.addColumn(Bytes.toBytes("Name"), Bytes.toBytes("lastName"), Bytes.toBytes("Gholami"));
-        put.addColumn(Bytes.toBytes("Name"), Bytes.toBytes("friends"), Bytes.toBytes(Arrays.toString(arr)));
+        Put put = new Put(Bytes.toBytes("myRow9"));
+        put.addColumn(Bytes.toBytes("Name"), Bytes.toBytes("firstName"), Bytes.toBytes("Steve"));
+        put.addColumn(Bytes.toBytes("Name"), Bytes.toBytes("lastName"), Bytes.toBytes("StevePoor"));
+        put.addColumn(Bytes.toBytes("Name"), Bytes.toBytes("friends"), WritableUtils.toByteArray(toWritable(new ArrayList<>(Arrays.asList(arr)))));
 //        table.put(put);
-        Get get = new Get(Bytes.toBytes("myRow4"));
+        Get get = new Get(Bytes.toBytes("myRow9"));
+//        CHECK IF A ROW EXISTS
         System.out.println(table.exists(get));
 //        Scan scan = new Scan();
         get.setMaxVersions(1);
         get.addFamily(Bytes.toBytes("Name"));
-
         get.addColumn(Bytes.toBytes("Name"), Bytes.toBytes("friends"));
-
-        Result result = table.get(get);
-        String[] strArr = castResultToStringArray(result, "Name", "friends");
-        for(String s: strArr){
-            System.out.println(s);
-        }
         System.out.println("Done!");
-
-        byte[] databytes = Bytes.toBytes("Name");
-//        System.out.println(getColumnsInColumnFamily(result, "Name")[0]);
-
+        Result result = table.get(get);
+        System.out.println(castResultToStringArrayList(result, "Name", "friends").get(0));
         table.close();
         connection.close();
     }
