2540fb0a413118e7c98a4cfd372b63daf3b2116b
ArashMoayedi <arash.moayedi1@gmail.com>
2017-08-20 12:23:43 +0430

    Kafka V.1

--- src/main/java/kon/shol/Main.java
+++ src/main/java/kon/shol/Main.java
@@ -4,48 +4,52 @@ package kon.shol;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class Main {
-    static LinkedBlockingQueue<String> consumerQueue = new LinkedBlockingQueue<String>();
-    static LinkedBlockingQueue<String> producerQueue = new LinkedBlockingQueue<String>();
-    public static void main(String[] args) throws InterruptedException {
-        Thread[] threads = new Thread[60];
-        Thread consumer = new Thread(new Consumer("0", "Crawler"));
-        Thread producer = new Thread(new Producer());
-        consumerQueue.put("http://www.moz.com/top500");
-        for (Thread thread : threads) {
-            thread = new Thread(new Crawler() {
-                @Override
-                public String getLink() {
-                    try {
-                        return consumerQueue.take();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                        return null;
-                    }
-                }
-                @Override
-                public void sendLink(String link) {
-                    try {
-                        producerQueue.put(link);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            });
-            thread.start();
-        }
-        consumer.start();
-        producer.start();
-        for (Thread thread : threads) {
-            try {
-                thread.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
 
-        }
-        consumer.join();
-        producer.join();
+    public static void main(String[] args) throws InterruptedException {
+
+        Producer producer = new Producer("urls");
+        Consumer consumer = new Consumer("0", "urls");
+
+//        Thread[] threads = new Thread[60];
+//        producer.sendLink("http://www.moz.com/top500");
+//        for (Thread thread : threads) {
+//            thread = new Thread(new Crawler() {
+//                @Override
+//                public String getLink() {
+//                    try {
+//                        return consumerQueue.take();
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                        return null;
+//                    }
+//                }
+//
+//                @Override
+//                public void sendLink(String link) {
+//                    try {
+//                        producerQueue.put(link);
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//            });
+//            thread.start();
+//        }
+//        consumer.start();
+//        producer.start();
+//        for (Thread thread : threads) {
+//            try {
+//                thread.join();
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//
+//        }
+//        consumer.join();
+//        producer.join();
+
     }
 }
+
 
 
