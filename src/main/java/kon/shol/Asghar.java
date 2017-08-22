23da88d72a3f424401558edfe76f43b228fbf839
ArashMoayedi <arash.moayedi1@gmail.com>
2017-08-22 11:19:49 +0430

    Fixed asghar! never change asghar! asghar is the best. asghar is bulk asghar is index mikone doroste

--- src/main/java/kon/shol/Asghar.java
+++ src/main/java/kon/shol/Asghar.java
@@ -1,29 +1,16 @@
 package kon.shol;
 
 
-import kon.shol.KafkaClass;
-
-import static kon.shol.Main.queue;
 
 public class Asghar extends Crawler {
 
     @Override
     public void sendLink(String link) {
-        try {
-            queue.put(link);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        KafkaClass.sendLink(link);
     }
 
     @Override
     public String getLink() {
-        try {
-            return queue.take();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-            return null;
-        }
+        return KafkaClass.getLink();
     }
-
 }
