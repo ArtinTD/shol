4cc5b31d5ba69067e7fc3ac3e92dab21a4af8440
Hossein Ahmadzadeh <h.ahmadzadeh1998@gmail.com>
2017-08-22 10:00:12 +0430

    Initialize Thread Manager

--- src/main/java/kon/shol/Asghar.java
+++ src/main/java/kon/shol/Asghar.java
@@ -3,16 +3,27 @@ package kon.shol;
 
 import kon.shol.KafkaClass;
 
+import static kon.shol.Main.queue;
+
 public class Asghar extends Crawler {
 
     @Override
     public void sendLink(String link) {
-        KafkaClass.sendLink(link);
+        try {
+            queue.put(link);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public String getLink() {
-        return KafkaClass.getLink();
+        try {
+            return queue.take();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
 }
