96a281ed2589040d58030d48e0166e19ed9c6e63
mohammadreza <ghobakhlou.mohammadreza@gmail.com>
2017-08-30 16:14:11 +0430

    add manitor package

---
+++ src/main/java/kon/shol/searchengine/monitor/Monitor.java
@@ -0,0 +1,13 @@
+package kon.shol.searchengine.monitor;
+
+public class Monitor{
+
+    static int speed;
+    static int numberOfFetchedLinksFromQueueToCrawl;
+    static int numberOfPoliteDomains;
+    static int numberOfenglishLinks;
+    static int numberOfCrawledLinks;
+    static int numberOfActiveThreads;
+    static int allLinkeCrawled;
+
+}
