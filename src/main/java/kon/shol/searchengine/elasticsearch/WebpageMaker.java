c631eeff2f7d332b8039ea8cd7f201a4be904cba
Mohammadamin  Karbasforushan <k.mohammadamin@gmail.com>
2017-09-03 15:55:24 +0430

    set threadCount to 16 in MultiThreadEsFeeder

--- src/main/java/kon/shol/searchengine/elasticsearch/WebpageMaker.java
+++ src/main/java/kon/shol/searchengine/elasticsearch/WebpageMaker.java
@@ -8,12 +8,12 @@ public class WebpageMaker {
    public static WebPage makeWebpage(Result r) {
       
       String url = Bytes.toString(r.getRow());
-      String title = Bytes.toString(r.getValue(Bytes.toBytes("data"), Bytes.toBytes("title")));
-      String text = Bytes.toString(r.getValue(Bytes.toBytes("data"), Bytes.toBytes("text")));
+      String title = Bytes.toString(r.getValue(Bytes.toBytes("data"), Bytes.toBytes("title      ")));
+      String text = Bytes.toString(r.getValue(Bytes.toBytes("data"), Bytes.toBytes("text       ")));
       String desc = Bytes.toString(r.getValue(Bytes.toBytes("data"), Bytes.toBytes("description")));
-      String h1h3 = Bytes.toString(r.getValue(Bytes.toBytes("data"), Bytes.toBytes("h1h3")));
-      String h4h6 = Bytes.toString(r.getValue(Bytes.toBytes("data"), Bytes.toBytes("h4h6")));
-      String imagesAlt = Bytes.toString(r.getValue(Bytes.toBytes("data"), Bytes.toBytes("alt")));
+      String h1h3 = Bytes.toString(r.getValue(Bytes.toBytes("data"), Bytes.toBytes("h1h3       ")));
+      String h4h6 = Bytes.toString(r.getValue(Bytes.toBytes("data"), Bytes.toBytes("h4h6       ")));
+      String imagesAlt = Bytes.toString(r.getValue(Bytes.toBytes("data"), Bytes.toBytes("alt          ")));
       
       String anchorTexts;
       try {
