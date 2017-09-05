c69353df1ab2de2ca1c37a58536bf08a492400c1
Artin Tajdini <artin.taj@gmail.com>
2017-09-05 12:25:49 +0430

    hashing

--- src/main/java/kon/shol/searchengine/sparkmapreduce/PageRank.java
+++ src/main/java/kon/shol/searchengine/sparkmapreduce/PageRank.java
@@ -15,6 +15,7 @@ import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.sources.In;
 import org.apache.spark.storage.StorageLevel;
 import scala.Tuple2;
 
@@ -47,25 +48,30 @@ public class PageRank {
                 ImmutableBytesWritable.class,
                 Result.class);
 
-        hBaseRDD.persist(StorageLevel.MEMORY_AND_DISK());
-        JavaPairRDD<String, ArrayList<String>> links = hBaseRDD.mapToPair(s -> {
+        JavaPairRDD<Integer, String> revertedHash = hBaseRDD.mapToPair( s -> {
             Result r = s._2;
             String row = Bytes.toString(r.getRow());
-            ArrayList<String> adj = new ArrayList<>();
-            r.getFamilyMap(Bytes.toBytes("anchors")).forEach((k, v) -> {
-                adj.add(Bytes.toString(k));
-            });
-            return new Tuple2<>(row , adj);
+            return new Tuple2<>(row.hashCode(), row);
         });
 
-        JavaPairRDD<String, Double> ranks = links.mapValues(rs -> 1.0);
+        JavaPairRDD<Integer, ArrayList<Integer>> links = hBaseRDD.mapToPair(s -> {
+            Result r = s._2;
+            String row = Bytes.toString(r.getRow());
+            ArrayList<Integer> adj = new ArrayList<>();
+            r.getFamilyMap(Bytes.toBytes("anchors")).forEach((k, v) -> {
+                adj.add(Bytes.toString(k).hashCode());
+            });
+            return new Tuple2<>(row.hashCode() , adj);
+        });
+
+        JavaPairRDD<Integer, Double> ranks = links.mapValues(rs -> 1.0);
 
         for (int current = 0; current < 10; current++) {
-            JavaPairRDD<String, Double> contribs = links.join(ranks).values()
+            JavaPairRDD<Integer, Double> contribs = links.join(ranks).values()
                     .flatMapToPair(s -> {
                         int urlCount = Iterables.size(s._1());
-                        List<Tuple2<String, Double>> results = new ArrayList<>();
-                        for (String n : s._1) {
+                        List<Tuple2<Integer, Double>> results = new ArrayList<>();
+                        for (int n : s._1) {
                             results.add(new Tuple2<>(n, s._2() / urlCount));
                         }
                         return results.iterator();
@@ -73,16 +79,15 @@ public class PageRank {
             ranks = contribs.reduceByKey((k, v) -> k + v).mapValues(sum -> 0.15 + sum * 0.85);
         }
 
-        JavaPairRDD<ImmutableBytesWritable, Put> hbasePuts = ranks.mapToPair(s -> {
-            Put put = new Put(Bytes.toBytes(s._1));
-            put.addColumn(Bytes.toBytes("data"), Bytes.toBytes("pagerank"), Bytes.toBytes(s._2));
+        JavaPairRDD<ImmutableBytesWritable, Put> hbasePuts = ranks.join(revertedHash).mapToPair(s -> {
+            Put put = new Put(Bytes.toBytes(s._2._2));
+            put.addColumn(Bytes.toBytes("data"), Bytes.toBytes("pagerank"), Bytes.toBytes(s._2._1));
             return new Tuple2<>(new ImmutableBytesWritable(), put);
         });
 
         hbasePuts.saveAsNewAPIHadoopDataset(conf);
 
         sc.stop();
-
     }
 
     private static String convertScanToString(Scan scan) {
